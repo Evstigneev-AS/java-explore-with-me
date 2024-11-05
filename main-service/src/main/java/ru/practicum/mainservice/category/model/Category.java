8022cff0db3004b0672dcab77da8da75ba2e19d2
Anatolii <Steg92sm13@yandex.ru>
2024-11-05 19:46:39 +0800

    main_svc реализация

---
+++ main-service/src/main/java/ru/practicum/mainservice/category/model/Category.java
@@ -0,0 +1,21 @@
+package ru.practicum.mainservice.category.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
